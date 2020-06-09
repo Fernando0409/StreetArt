d36586c6441db61ebea67d1745358355c71ed0a0
Luis Fernando <fernando04_software@outlook.com>
2020-06-08 22:18:40 -0500

    Log out & login files
    Se añade archivo para cerrar sesion y para iniciar sesion con usuario o email

--- logica/funcionalidades/ingresar.php
+++ logica/funcionalidades/ingresar.php
@@ -2,27 +2,32 @@
     require_once '../root.php';
     session_start();
 
-    $errors = array();
+    $user = false;
 
     $correo = mysqli_real_escape_string($conn, $_POST['email']);
     $pass = mysqli_real_escape_string($conn, $_POST['password']);
 
     if(!empty($correo) && !empty($pass)){
-        
-        $query = "SELECT * FROM user WHERE email='$correo' OR username='$correo' AND password='$pass'";
-        $results = mysqli_query($conn, $query);
+      $query = "SELECT COUNT(*) AS contar FROM user WHERE email = '{$correo}' AND password='{$pass}'";
 
-        if (mysqli_num_rows($results) == 1 ) {
-            
+      $result = mysqli_query($conn, $query);
+      $array = mysqli_fetch_array($result);
+
+      if($array['contar'] > 0) {
+          $_SESSION['email'] = $correo;
+          header('location: ../../index.php');
+      } else {
+          $query = "SELECT COUNT(*) AS contar FROM user WHERE username = '{$correo}' AND password='{$pass}'";
+
+          $result = mysqli_query($conn, $query);
+          $array = mysqli_fetch_array($result);
+
+          if($array['contar'] > 0){
             $_SESSION['email'] = $correo;
-            $_SESSION['success'] = "You are now logged in";
-            echo "Iniciando sesion... <br>";
-            echo $correo."<br>";
-            echo $pass;
-            //header('location: perfil.php');
-
-          } else
-            echo "Correo electronico o contraseña erroneos";
+            header('location: ../../index.php');
+          } else 
+              echo "Verifica los datos";
+      }
     } else
         echo "No se ingreso email o contraseña";
     
