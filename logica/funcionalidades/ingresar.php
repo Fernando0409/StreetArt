a944ded45276285146dcc1b086fcb32ef14cc66e
Luis Fernando <fernando04_software@outlook.com>
2020-06-10 03:21:16 -0500

    Update sesion  por medio de email & username
    Se realiza la verificacion de si inicio sesion con el email o username. En caso de haberlo hecho con el email devuelve el username

--- logica/funcionalidades/ingresar.php
+++ logica/funcionalidades/ingresar.php
@@ -4,26 +4,37 @@
 
     $user = false;
 
-    $correo = mysqli_real_escape_string($conn, $_POST['email']);
+    $correo = mysqli_real_escape_string($conn, $_POST['email']);  // Puede ser el email o username
     $pass = mysqli_real_escape_string($conn, $_POST['password']);
 
-    if(!empty($correo) && !empty($pass)){
+    if(!empty($correo) && !empty($pass)){ // Si no estan vacios
+
+      // Consulta si el usuario se logeo con el email
       $query = "SELECT COUNT(*) AS contar FROM user WHERE email = '{$correo}' AND password='{$pass}'";
 
       $result = mysqli_query($conn, $query);
       $array = mysqli_fetch_array($result);
 
+      // Si es asi va a recuperar el username que tiene ese correo
       if($array['contar'] > 0) {
-          $_SESSION['email'] = $correo;
+        $username = "";
+        $sql = "SELECT * FROM user WHERE email = '$correo'";
+        $result = mysqli_query($conn, $sql);
+        
+        while($users = mysqli_fetch_array($result)){
+            $username = $users['username'];
+        }
+          $_SESSION['username'] = $username;
           header('location: ../../index.php');
-      } else {
+      } 
+      else { // De lo contrario revisara si se logeo con el username
           $query = "SELECT COUNT(*) AS contar FROM user WHERE username = '{$correo}' AND password='{$pass}'";
 
           $result = mysqli_query($conn, $query);
           $array = mysqli_fetch_array($result);
 
           if($array['contar'] > 0){
-            $_SESSION['email'] = $correo;
+            $_SESSION['username'] = $correo;
             header('location: ../../index.php');
           } else 
               echo "Verifica los datos";
